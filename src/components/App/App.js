774122d27aaf482802888eb64a32b723467a5b6a
Laura Unaeze <l.unaeze5@gmail.com>
2017-12-06 11:37:26 -0700

    Added welcome message functionality.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,20 +16,46 @@ class App extends Component {
   }
 
   render() {
+    let welcomeMessage;
+    if (this.state.firstName) {
+      welcomeMessage = `Welcome, ${this.state.firstName} `;
+    }
+
+    if (this.state.lastName) {
+      if (!welcomeMessage) {
+        welcomeMessage = "Welcome, ";
+      }
+      // this adds the last name to the welcome message
+      welcomeMessage += this.state.lastName;
+    }
+
+    // this takes away the space at the end of the first name in the initial welcome message
+    // and adds an exlcamation point after the first name if no last name exists
+    if (welcomeMessage) {
+      welcomeMessage = welcomeMessage.trim() + "!";
+    }
+
     return (
       <div>
-        <input 
-          type="text"
-          name="firstName"
-          onChange={ this.handleInputChanged}
-          value={this.state.firstName}
-        />
-        <input 
-          type="text"
-          name="lastName"
-          onChange={ this.handleInputChanged }
-          value={this.state.lastName}
-        />
+        <form>
+          <label htmlFor="firstName">First Name:</label>
+          <input 
+            type="text"
+            name="firstName"
+            onChange={this.handleInputChanged}
+            value={this.state.firstName}
+          />
+          <label htmlFor="lastName">Last Name:</label>
+          <input 
+            type="text"
+            name="lastName"
+            onChange={this.handleInputChanged}
+            value={this.state.lastName}
+          />
+        </form>
+        <div>
+          <p>{welcomeMessage}</p>
+        </div>
 
       </div>
     );
